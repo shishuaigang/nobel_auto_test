af0c2fe462b30510b2f0ee7f933b01949078f2fe
shishuaigang <shishuaigang@163.com>
2017-07-06 16:47:22 +0800

    完善case

---
+++ src/nobel/auto/test/testcase/ToCheck.java
@@ -0,0 +1,32 @@
+package nobel.auto.test.testcase;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.NoSuchElementException;
+
+import static nobel.auto.test.page.Home.*;
+import static nobel.auto.test.page.Unlock.unlock;
+import static nobel.auto.test.page.ZhiJian.preQCscan;
+
+/**
+ * Created by shishuaigang on 2017/7/6.
+ * 送检case
+ */
+
+public class ToCheck extends CommonCase{
+
+    public ToCheck(AndroidDriver d){
+        super(d);
+    }
+
+    public void SongJian() {
+        zhijian.click();
+        preQCscan.click();
+
+        //画面解锁
+        try {
+            unlock.click();
+        } catch (NoSuchElementException e) {
+            System.out.println("已解锁，不需要使用刷卡解锁");
+        }
+    }
+}
